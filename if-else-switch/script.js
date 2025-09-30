c15d66fcc842bb81101e4090f452f7ad79be9339
Eduarda <eduardalunardipereira759@gmail.com>
2025-09-30 08:34:13 -0300

    ..

--- if-else-switch/script.js
+++ if-else-switch/script.js
@@ -2,7 +2,21 @@
 let curso = "JavaScript";
 let aluno= "Eduarda";
 
-console.log ("O aluno" + aluno + "está aprendendo" + curso + "JavaScript.");
+let mensagem1 ="O aluno" + aluno + "está aprendendo" + curso + "JavaScript.";
+console.log(mensagem1);
+
+let mensagem2 = "O aluno" ${aluno} "está aprendendo" ${curso} ".";
+console.log(mensagem2);
 
 // --- Parte 2 ---
-let saldo = 1000
+let saldo = 1000;
+let deposito= 500;
+let saque= 300; 
+
+console.log ("Seu saldo é: R$" + saldo)
+
+saldo = saldo + deposito
+console.log ("Saldo após depósito de R$" + deposito + ":R$" + saldo);
+
+saldo = saldo + saque
+console.log ("Saldo após saque de R$" + saque + "R$" + saldo);
