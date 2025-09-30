c0746e700c33fc2a46947ed9f500a97e2a75b587
Eduarda <eduardalunardipereira759@gmail.com>
2025-09-30 08:14:23 -0300

    index

---
+++ if-else-switch/script.js
@@ -0,0 +1,8 @@
+// --- Parte 1 ---
+let curso = "JavaScript";
+let aluno= "Eduarda";
+
+console.log ("O aluno" + aluno + "está aprendendo" + curso + "JavaScript.");
+
+// --- Parte 2 ---
+let saldo = 1000
